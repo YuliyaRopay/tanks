83411edb8c1609c449fb1074583afaf24fa6f167
YuliyaRopay <jcolor@ukr.net>
2016-01-17 21:41:43 +0200

    launcher+splash

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,11 +7,9 @@ public class Launcher {
 
     public static void main(String[] args) throws Exception {
 
-        /*
         SplashScreen splash=SplashScreen.getSplashScreen();
         Thread.sleep(5000);
         splash.close();
-        */
 
         ActionField af = new ActionField();
         af.runTheGame();
